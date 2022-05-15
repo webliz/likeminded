e21e7387268cdab81e54951e96d26d939863d960
webliz <lizafox@outlook.com>
2022-05-15 22:27:41 +0200

    Add form validation types

--- src/components/App.types.ts
+++ src/components/App.types.ts
@@ -5,11 +5,50 @@ interface User {
   password: string,
   role: number
 }
-  
+
 export const initialUser: User = {
   username: "",
   email: "",
   password: "",
   role: 0
 }
+
+/** Validation */
+
+interface FormValidation {
+  username: {
+    required: boolean,
+    max: boolean,
+  },
+  email: {
+    required: boolean,
+    valid: boolean
+  },
+  password: {
+    required: boolean,
+    min: boolean,
+    valid: boolean
+  },
+  role: {
+    required: boolean
+  }
+}
   
+export const initialValidation: FormValidation = {
+  username: {
+    required: false,
+    max: false
+  },
+  email: {
+    required: false,
+    valid: true
+  },
+  password: {
+    required: false,
+    min: false,
+    valid: true
+  },
+  role: {
+    required: false
+  }
+}
