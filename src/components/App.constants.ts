c3813f4397e879549605fe1ba35f2c7b2f2f20a2
webliz <lizafox@outlook.com>
2022-05-15 22:20:53 +0200

    Add email and password regular expressions

--- src/components/App.constants.ts
+++ src/components/App.constants.ts
@@ -1,2 +1,6 @@
 
 export const userRoles = [{key: 0, name: "Please select a user role"}, {key: 1, name: "User"}, {key: 2, name: "Mentor"}, {key: 3, name: "Autor"}];
+/** Regular expressions */
+export const EMAIL_REGEX:RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const PASSWORD_STRONG_REGEX:RegExp = /(?=.*[A-Z])(?=.*[0-9])(?=(.*[^A-Za-z0-9]){2})(?=.{8,})/;
+export const PASSWORD_REGEX:RegExp = /(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
