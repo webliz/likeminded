05b5b479ec3e923bcbc51d7c8254175fcae91dc8
webliz <lizafox@outlook.com>
2022-05-15 21:53:22 +0200

    Add application types

---
+++ src/components/App.types.ts
@@ -0,0 +1,15 @@
+
+interface User {
+  username: string,
+  email: string,
+  password: string,
+  role: number
+}
+  
+export const initialUser: User = {
+  username: "",
+  email: "",
+  password: "",
+  role: 0
+}
+  
